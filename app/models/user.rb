191d84da659ffe158c3623a2a11b2be3129d7694
Uthaeus <romanlavery@gmail.com>
2023-05-07 15:09:22 -0600

    changed password in database

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
     validates_uniqueness_of :email, :username
     after_initialize :set_role
     has_secure_password validations: false
-    alias_attribute :password_digest, :password
 
     has_many :meetups
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_07_191849) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_07_210738) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,10 +31,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_07_191849) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "username"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role"
+    t.string "password_digest"
   end
 
   add_foreign_key "meetups", "users"
