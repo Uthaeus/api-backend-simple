842cc010cefe0f46aa7a9d906cd7a4cf6e703bd4
Uthaeus <romanlavery@gmail.com>
2023-05-07 13:28:05 -0600

    added roles to users, created after_initialize function for setting default role

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:email, :username, :password)
+      params.require(:user).permit(:email, :username, :role, :password)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+    validates_presence_of :email, :username
+    validates_uniqueness_of :email, :username
+    after_initialize :set_role
+
+    has_many :meetups
+
+    def set_role
+        self.role ||= :user
+    end
+
 end
